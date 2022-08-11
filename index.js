bb19b41e195ce30b68369b6b06185929e9eeb3cf
kikigoestech <101837197+kikigoestech@users.noreply.github.com>
2022-08-11 18:20:48 -0400

    first questions added

--- index.js
+++ index.js
@@ -4,7 +4,34 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project use?',
+        choices: ['None', 'Apache 2.0', 'MIT', 'GPL v3.0'],
+        validate: licenseInput = () => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('Please select one of the four options')
+                return false;
+                }
+        }
+    }]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
