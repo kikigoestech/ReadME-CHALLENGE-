8b9e97c656388a45fdb352f537e00df0e71ba23c
kikigoestech <101837197+kikigoestech@users.noreply.github.com>
2022-08-11 18:35:48 -0400

    renderlicensebadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,23 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const index = require('../index.js');
+
+// function that returns a license badge based on which license is passed in
+// If there is no license, it returns an empty string
+function renderLicenseBadge(license) {
+  let badge = '';
+  if(license === 'MIT') {
+      badge = '![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)'
+  } else if (license === 'Apache 2.0') {
+      badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+  } else if (license === 'GPL v3.0') {
+      badge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+  } else {
+    badge = ''
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
