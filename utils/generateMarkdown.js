545506fefe4cafcb46faa2e6f7ddf7e13ddf4c88
kikigoestech <101837197+kikigoestech@users.noreply.github.com>
2022-08-11 18:43:22 -0400

    markdown generated

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -49,10 +49,36 @@ function renderLicenseSection(license) {
 }
 
 // TODO: function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answer) {
 
+  return`
+  # ${answer.title}
+  ## ${renderLicenseSection(answer.license)} ${renderLicenseBadge(answer.license)}
+  ### ${renderLicenseLink(answer.license)}
+  ## Table of Contents:
+  ###  * [License](#license)
+  ###  * [Installation](#installation)
+  ###  * [Usage](#usage)
+  ###  * [Contributors](#contributors)
+  ###  * [Tests](#tests)
+  ###  * [Questions](#questions)
+  ## Installation:
+  ### You must install the following for this app to function:
+  ### ${answer.installation}
+  ## Usage:
+  ### ${answer.usage}
+  ## Contributors:
+  ### ${answer.contributions}
+  ## Tests:
+  ### Run the following commands in your terminal to test this app:
+  ### ${answer.test}
+  ## Questions:
+  ### If you have any questions, you may contact me at either
+  ### Github: https://github.com/${answer.askMe}
+  ### or
+  ### Email: ${answer.email}
 `;
 }
 
+// exports
 module.exports = generateMarkdown;
